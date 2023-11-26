3487bb84655d19b01939086ce66cce252e596f46
Mahammad-Rehan <149800722+Mahammad-Rehan@users.noreply.github.com>
2023-11-26 15:15:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import BlogList from './components/BlogList';
+import CreateBlogPost from './components/CreateBlogPost';
+import UpdateBlogPost from './components/UpdateBlogPost';
+import DeleteBlogPost from './components/DeleteBlogPost';
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/create">Create Post</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<BlogList />} />
+        <Route path="/create" element={<CreateBlogPost />} />
+        <Route path="/posts/:id/update" element={<UpdateBlogPost />} />
+        <Route path="/posts/:id/delete" element={<DeleteBlogPost />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
